c4dc9cecdb9469a40b076c6f8038c0d9fc759768
fx-k <admin@fxit.top>
2026-01-18 22:28:52 +0800

    refactor: remove version and commitId display from settings page
    - Eliminated the display of version number and commit ID from the settings page for a cleaner UI.

--- front/pages/sys/settings.vue
+++ front/pages/sys/settings.vue
@@ -1,10 +1,6 @@
 <template>
   <Header :user="currentUser"/>
   <div class="space-y-4  flex flex-col p-4 my-4 dark:bg-neutral-800">
-    <div class="flex flex-col items-end text-xs text-gray-400">
-      <div v-if="version" class="w-32">版本号: {{ version }}</div>
-      <div v-if="commitId" class="w-32">commitId: {{ commitId }}</div>
-    </div>
     <UFormGroup label="管理员账号" name="adminUserName" :ui="{label:{base:'font-bold'}}">
       <UInput v-model="state.adminUserName"/>
     </UFormGroup>
